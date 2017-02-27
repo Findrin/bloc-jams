6951dca11ce5c589bdf34971f6b192a0eb2833ac
Findrin <raidis86@gmail.com>
2017-02-26 22:41:36 -0600

    Assignment for CH.9

--- scripts/landing.js
+++ scripts/landing.js
@@ -17,12 +17,11 @@ var revealPoint = function(point) {
          animatePoints(pointsArray);
      }
 
-     var sellingPoints = document.getElementsByClassName("selling-points")[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
         window.addEventListener("scroll", function(event) {
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+          if (pointsArray[0].getBoundingClientRect().top <= 500) {
              animatePoints(pointsArray);   
          }
      });
  }
+ 
+ 
